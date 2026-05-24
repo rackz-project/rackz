baa8d9eee6f0161926fabd86217c25b8c50f45d6
sploitzberg <sploitzberg@proton.me>
2026-05-24 16:56:29 +0100

    daemon: rebrand all remaining Monero terminal output strings to Rackz
    main.cpp: version/help banners
    
    command_server.cpp: get_commands_str header
    
    command_line_args.h: Windows service name
    
    rpc_command_executor.cpp: mining reward units, status strings
    
    command_parser_executor.cpp: prune warning messages

--- src/daemon/command_line_args.h
+++ src/daemon/command_line_args.h
@@ -1,21 +1,21 @@
 // Copyright (c) 2014-2024, The Monero Project
-// 
+//
 // All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without modification, are
 // permitted provided that the following conditions are met:
-// 
+//
 // 1. Redistributions of source code must retain the above copyright notice, this list of
 //    conditions and the following disclaimer.
-// 
+//
 // 2. Redistributions in binary form must reproduce the above copyright notice, this list
 //    of conditions and the following disclaimer in the documentation and/or other
 //    materials provided with the distribution.
-// 
+//
 // 3. Neither the name of the copyright holder nor the names of its contributors may be
 //    used to endorse or promote products derived from this software without specific
 //    prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
 // EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
 // MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
@@ -36,62 +36,45 @@
 
 namespace daemon_args
 {
-  std::string const WINDOWS_SERVICE_NAME = "Monero Daemon";
+  std::string const WINDOWS_SERVICE_NAME = "Rackz Daemon";
 
   const command_line::arg_descriptor<std::string, false, true, 2> arg_config_file = {
-    "config-file"
-  , "Specify configuration file"
-  , (daemonizer::get_default_data_dir() / std::string(CRYPTONOTE_NAME ".conf")).string()
-  , {{ &cryptonote::arg_testnet_on, &cryptonote::arg_stagenet_on }}
-  , [](std::array<bool, 2> testnet_stagenet, bool defaulted, std::string val)->std::string {
+    "config-file",
+    "Specify configuration file",
+    (daemonizer::get_default_data_dir() / std::string(CRYPTONOTE_NAME ".conf")).string(),
+    {{&cryptonote::arg_testnet_on, &cryptonote::arg_stagenet_on}},
+    [](std::array<bool, 2> testnet_stagenet, bool defaulted, std::string val) -> std::string
+    {
       if (testnet_stagenet[0] && defaulted)
-        return (daemonizer::get_default_data_dir() / "testnet" /
-                std::string(CRYPTONOTE_NAME ".conf")).string();
+        return (daemonizer::get_default_data_dir() / "testnet" / std::string(CRYPTONOTE_NAME ".conf")).string();
       else if (testnet_stagenet[1] && defaulted)
-        return (daemonizer::get_default_data_dir() / "stagenet" /
-                std::string(CRYPTONOTE_NAME ".conf")).string();
+        return (daemonizer::get_default_data_dir() / "stagenet" / std::string(CRYPTONOTE_NAME ".conf")).string();
       return val;
-    }
-  };
+    }};
   const command_line::arg_descriptor<std::string, false, true, 2> arg_log_file = {
-    "log-file"
-  , "Specify log file"
-  , (daemonizer::get_default_data_dir() / std::string(CRYPTONOTE_NAME ".log")).string()
-  , {{ &cryptonote::arg_testnet_on, &cryptonote::arg_stagenet_on }}
-  , [](std::array<bool, 2> testnet_stagenet, bool defaulted, std::string val)->std::string {
+    "log-file",
+    "Specify log file",
+    (daemonizer::get_default_data_dir() / std::string(CRYPTONOTE_NAME ".log")).string(),
+    {{&cryptonote::arg_testnet_on, &cryptonote::arg_stagenet_on}},
+    [](std::array<bool, 2> testnet_stagenet, bool defaulted, std::string val) -> std::string
+    {
       if (testnet_stagenet[0] && defaulted)
-        return (daemonizer::get_default_data_dir() / "testnet" /
-                std::string(CRYPTONOTE_NAME ".log")).string();
+        return (daemonizer::get_default_data_dir() / "testnet" / std::string(CRYPTONOTE_NAME ".log")).string();
       else if (testnet_stagenet[1] && defaulted)
-        return (daemonizer::get_default_data_dir() / "stagenet" /
-                std::string(CRYPTONOTE_NAME ".log")).string();
+        return (daemonizer::get_default_data_dir() / "stagenet" / std::string(CRYPTONOTE_NAME ".log")).string();
       return val;
-    }
-  };
-  const command_line::arg_descriptor<std::size_t> arg_max_log_file_size = {
-    "max-log-file-size"
-  , "Specify maximum log file size [B]"
-  , MAX_LOG_FILE_SIZE
-  };
+    }};
+  const command_line::arg_descriptor<std::size_t> arg_max_log_file_size = {"max-log-file-size",
+                                                                           "Specify maximum log file size [B]",
+                                                                           MAX_LOG_FILE_SIZE};
   const command_line::arg_descriptor<std::size_t> arg_max_log_files = {
-    "max-log-files"
-  , "Specify maximum number of rotated log files to be saved (no limit by setting to 0)"
-  , MAX_LOG_FILES
-  };
-  const command_line::arg_descriptor<std::string> arg_log_level = {
-    "log-level"
-  , "0-4 or categories"
-  , ""
-  };
-  const command_line::arg_descriptor<std::vector<std::string>> arg_command = {
-    "daemon_command"
-  , "Hidden"
-  };
-  const command_line::arg_descriptor<unsigned> arg_max_concurrency = {
-    "max-concurrency"
-  , "Max number of threads to use for a parallel job"
-  , 0
-  };
+    "max-log-files", "Specify maximum number of rotated log files to be saved (no limit by setting to 0)",
+    MAX_LOG_FILES};
+  const command_line::arg_descriptor<std::string> arg_log_level = {"log-level", "0-4 or categories", ""};
+  const command_line::arg_descriptor<std::vector<std::string>> arg_command = {"daemon_command", "Hidden"};
+  const command_line::arg_descriptor<unsigned> arg_max_concurrency = {"max-concurrency",
+                                                                      "Max number of threads to use for a parallel job",
+                                                                      0};
 
   const command_line::arg_descriptor<std::string> arg_proxy = {
     "proxy",
@@ -104,52 +87,39 @@ namespace daemon_args
     false,
   };
   const command_line::arg_descriptor<bool> arg_public_node = {
-    "public-node"
-  , "Allow other users to use the node as a remote (restricted RPC mode, view-only commands) and advertise it over P2P"
-  , false
-  };
+    "public-node",
+    "Allow other users to use the node as a remote (restricted RPC mode, view-only commands) and advertise it over P2P",
+    false};
 
-  const command_line::arg_descriptor<std::string> arg_zmq_rpc_bind_ip   = {
-    "zmq-rpc-bind-ip"
-      , "IP for ZMQ RPC server to listen on"
-      , "127.0.0.1"
-  };
+  const command_line::arg_descriptor<std::string> arg_zmq_rpc_bind_ip = {"zmq-rpc-bind-ip",
+                                                                         "IP for ZMQ RPC server to listen on",
+                                                                         "127.0.0.1"};
 
   const command_line::arg_descriptor<std::string, false, true, 2> arg_zmq_rpc_bind_port = {
-    "zmq-rpc-bind-port"
-  , "Port for ZMQ RPC server to listen on"
-  , std::to_string(config::ZMQ_RPC_DEFAULT_PORT)
-  , {{ &cryptonote::arg_testnet_on, &cryptonote::arg_stagenet_on }}
-  , [](std::array<bool, 2> testnet_stagenet, bool defaulted, std::string val)->std::string {
+    "zmq-rpc-bind-port",
+    "Port for ZMQ RPC server to listen on",
+    std::to_string(config::ZMQ_RPC_DEFAULT_PORT),
+    {{&cryptonote::arg_testnet_on, &cryptonote::arg_stagenet_on}},
+    [](std::array<bool, 2> testnet_stagenet, bool defaulted, std::string val) -> std::string
+    {
       if (testnet_stagenet[0] && defaulted)
         return std::to_string(config::testnet::ZMQ_RPC_DEFAULT_PORT);
       if (testnet_stagenet[1] && defaulted)
         return std::to_string(config::stagenet::ZMQ_RPC_DEFAULT_PORT);
       return val;
-    }
-  };
+    }};
   const command_line::arg_descriptor<std::vector<std::string>> arg_zmq_pub = {
-    "zmq-pub"
-  , "Address for ZMQ pub - tcp://ip:port or ipc://path"
-  };
+    "zmq-pub", "Address for ZMQ pub - tcp://ip:port or ipc://path"};
 
   const command_line::arg_descriptor<bool> arg_restricted_zmq_rpc = {
-    "restricted-zmq-rpc"
-  , "Restrict ZMQ RPC by disabling some sensitive methods; does not guarantee filtering of sensitive data"
-  , false
-  };
+    "restricted-zmq-rpc",
+    "Restrict ZMQ RPC by disabling some sensitive methods; does not guarantee filtering of sensitive data", false};
 
   const command_line::arg_descriptor<bool> arg_confirm_zmq_rpc_external_bind = {
-    "confirm-zmq-rpc-external-bind"
-  , "Confirm zmq-rpc-bind-ip value is NOT a loopback (local) IP"
-  , false
-  };
+    "confirm-zmq-rpc-external-bind", "Confirm zmq-rpc-bind-ip value is NOT a loopback (local) IP", false};
 
-  const command_line::arg_descriptor<bool> arg_zmq_rpc_disabled = {
-    "no-zmq"
-  , "Disable ZMQ RPC server"
-  };
+  const command_line::arg_descriptor<bool> arg_zmq_rpc_disabled = {"no-zmq", "Disable ZMQ RPC server"};
 
-}  // namespace daemon_args
+} // namespace daemon_args
 
 #endif // DAEMON_COMMAND_LINE_ARGS_H
